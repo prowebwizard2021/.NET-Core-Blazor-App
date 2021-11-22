e9a387c2f1db65b847e912649ab18b7953151026
Full stack <prowebwizard2021@gmail.com>
2021-11-22 18:10:39 +0800

    add project files

---
+++ Plugin.DataStore.InMemory/CategoryInMemoryRepository.cs
@@ -0,0 +1,62 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UseCases.DataStorePluginInterfaces;
+
+namespace Plugin.DataStore.InMemory
+{
+    public class CategoryInMemoryRepository : ICategoryRepository
+    {
+        private List<Category> categories;
+        public CategoryInMemoryRepository()
+        {
+            categories = new List<Category>()
+            {
+                new Category
+                {
+                    CategoryId = 1,
+                    Name = "Beverage",
+                    Description = "Beverage"
+                },
+                new Category
+                {
+                    CategoryId = 2,
+                    Name = "Meat",
+                    Description = "Meat"
+                },
+                new Category
+                {
+                    CategoryId = 3,
+                    Name = "Bakery",
+                    Description = "Bakery"
+                }
+            };
+        }
+
+        public void AddCategory(Category category)
+        {
+            if (categories.Any(x => x.Name.Equals(category.Name, StringComparison.OrdinalIgnoreCase))) return;
+            var maxId = categories.Max(x => x.CategoryId);
+            category.CategoryId = maxId + 1;
+            categories.Add(category);
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            var categoryToUpdate = GetCategoryById(category.CategoryId);
+            if (categoryToUpdate != null) categoryToUpdate = category;
+
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return categories;
+        }
+
+        public Category GetCategoryById(int categoryId)
+        {
+            return categories.FirstOrDefault(x => x.CategoryId == categoryId);
+        }
+    }
+}
---
+++ UseCases/AddCategoryUseCase.cs
@@ -0,0 +1,18 @@
+﻿using CoreBusiness;
+using UseCases.DataStorePluginInterfaces;
+
+namespace UseCases
+{
+    public class AddCategoryUseCase : IAddCategoryUseCase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        public AddCategoryUseCase(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public void Execute(Category category)
+        {
+            _categoryRepository.AddCategory(category);
+        }
+    }
+}
